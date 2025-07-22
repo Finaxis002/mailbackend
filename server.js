d3031991a202713ac5a208ae39bcbc31b81cb3b6
finaxis002 <finaxis.ai@gmail.com>
2025-07-22 15:50:26 +0530

    cors added

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const emailuser = require('./Routes/emailUser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 
 app.use('/api/email', emailuser);
